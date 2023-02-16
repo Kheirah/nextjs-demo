4cb21d89eec1daac25ab9b088817ea7468a74035
Ryan Ceres <c.nizz@web.de>
2023-02-16 11:19:33 +0100

    add styled components

---
+++ components/StyledButton.jsx
@@ -0,0 +1,25 @@
+import styled, { css } from "styled-components";
+
+export default styled.button`
+  all: unset;
+  border: none;
+  cursor: pointer;
+  padding: 1rem;
+  color: white;
+  ${({ color }) =>
+    color === "danger" &&
+    css`
+      background-color: var(--secondary-color);
+    `};
+
+  ${({ color }) =>
+    color === "success" &&
+    css`
+      background-color: var(--primary-success-color);
+    `};
+
+  &:hover {
+    color: black;
+    background-color: var(--primary-background);
+  }
+`;
